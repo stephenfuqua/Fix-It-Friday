bf714b529e41f7fcdcf2ada878e73dd981941e35
Stephen A. Fuqua <stephen@safnet.com>
2020-05-22 10:59:20 -0500

    ID for VCS root to match existing root in TC

--- .teamcity/_Self/vcsRoots/FixItFriday.kt
+++ .teamcity/_Self/vcsRoots/FixItFriday.kt
@@ -5,7 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object FixItFridayVcs : GitVcsRoot({
     name = "Fix-It-Friday"
-    url = "https://github.com/%github.organization%/Fix-It-Friday.git"
+    id("EdFi_AnalyticsTeam_FixItFriday_FixItFriday")
+    url = "https://github.com/%github.organization%/Fix-It-Friday"
     branch = "%git.branch.default%"
     branchSpec = """
         refs/heads/(*)
