cfc723e07cf26f4621f607a2188a7a7dcc112856
Stephen A. Fuqua <stephen@safnet.com>
2020-06-18 15:31:39 -0500

    switch to using the PR build feature

--- .teamcity/UI/buildTypes/PullRequestUIBuild.kt
+++ .teamcity/UI/buildTypes/PullRequestUIBuild.kt
@@ -2,6 +2,8 @@ package ui.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
@@ -14,19 +16,25 @@ object PullRequestUIBuild : BuildType ({
             id ="vcsTrigger"
             quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_CUSTOM
             quietPeriod = 120
-            triggerRules = "+:**"
-            branchFilter = "+:pull/*/head"
+            branchFilter = "+:*"
         }
     }
 
     features {
         commitStatusPublisher {
-            id = "BUILD_EXT_45"
             publisher = github {
                 githubUrl = "https://api.github.com"
                 authType = personalToken {
                     token = "%github.accessToken%"
                 }
+            }
+        }
+        pullRequests {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            provider = github {
+                authType = vcsRoot()
+                filterTargetBranch = "+:<default>"
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER_OR_COLLABORATOR
             }
         }
     }
