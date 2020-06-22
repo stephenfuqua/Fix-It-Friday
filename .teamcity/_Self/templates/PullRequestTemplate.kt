0d7d512c36cd9c0dc8ad3ec4197a7df206cb1582
Stephen A. Fuqua <stephen@safnet.com>
2020-06-22 12:51:37 -0500

    Trigger rules for PR build

--- .teamcity/_Self/templates/PullRequestTemplate.kt
+++ .teamcity/_Self/templates/PullRequestTemplate.kt
@@ -38,6 +38,7 @@ object PullRequestTemplate : BuildAndTestBaseClass() {
                 id ="vcsTrigger"
                 quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_CUSTOM
                 quietPeriod = 120
+                triggerRules = "+:%project.directory%"
                 // This allows triggering on "anything" and then removes
                 // triggering on the default branch and in feature branches,
                 // thus leaving only the pull requests.
