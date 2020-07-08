3709b34a1e60b03efdbe7a4f2bd5870269b3d878
Stephen A. Fuqua <stephen@safnet.com>
2020-07-08 08:34:10 -0500

    Finish conversion to ts/js from json

--- edfi.fif.api/src/ormconfig.ts
+++ edfi.fif.api/src/ormconfig.ts
@@ -1,11 +1,14 @@
-{
-  "type": "postgres",
-  "host": "localhost",
-  "port": 5432,
-  "username": "postgres",
-  "password": "pa55w0rd",
-  "database": "fixitfriday",
-  "entities": ["dist/**/*.entity.js"],
-  "synchronize": true,
-  "logging": true
-}
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+  type: 'postgres',
+  host: process.env.FIF_API_DB_HOST || 'localhost',
+  port: parseInt(process.env.FIF_API_DB_PORT, 10) || 5432,
+  username: process.env.FIF_API_DB_USERNAME || 'postgres',
+  password: process.env.FIF_API_DB_PASSWORD,
+  database: process.env.FIF_API_DB_DATABASE || 'fixitfriday',
+  entities: ['./**/*.entity.js'],
+  synchronize: true,
+};
